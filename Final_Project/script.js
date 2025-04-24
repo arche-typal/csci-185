b72febd3075db582906e1774a4dc04e1c271607d
Lily <archetypal.0@gmail.com>
2025-04-24 08:49:56 -0400

    Final Project

---
+++ Final_Project/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    navBar();
+  });
+
+function navBar() {
+    document.getElementById("top-nav").innerHTML = `
+        <a href="index.html">Home</a>
+        <a href="about.html">About</a>
+        <a href="faq.html">FAQ</a>
+        <a href="contact.html">Contact</a>
+        <a href="japan.html">Japan</a>
+        <a href="congo.html">Congo</a>
+        <a href="thailand.html">Thailand</a>
+        `;
+}

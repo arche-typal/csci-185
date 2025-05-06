c4e6b5440bc98c84f8a83f33bb2165a77c2aeaed
Lily <archetypal.0@gmail.com>
2025-05-06 01:38:14 -0400

    reset repo to commit before large .zip file. re-added contents from back-up.

--- Final_Project/script.js
+++ Final_Project/script.js
@@ -1,16 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-    navBar();
-  });
+  navBar();
+  footer();
+});
 
 function navBar() {
-    document.getElementById("top-nav").innerHTML = `
-        <a href="index.html">Home</a>
-        <a href="about.html">About</a>
-        <a href="faq.html">FAQ</a>
-        <a href="contact.html">Contact</a>
-        <a href="japan.html">Japan</a>
-        <a href="taiwan.html">Taiwan</a>
-        <a href="congo.html">Congo</a>
-        <a href="thailand.html">Thailand</a>
-        `;
+  document.getElementById("top-nav").innerHTML = `
+    <a href="index.html">Home</a>
+    <a href="about.html">About</a>
+    <a href="faq.html">FAQ</a>
+    <a href="contact.html">Contact</a>
+    <a href="japan.html">Japan</a>
+    <a href="taiwan.html">Taiwan</a>
+    <a href="congo.html">Congo</a>
+    <a href="thailand.html">Thailand</a>
+  `;
+}
+
+function footer() {
+  document.getElementById("footer").innerHTML = `
+    <small>&copy; 2025 Lily's Travel Blog. All Rights Reserved.</small>
+  `;
 }
